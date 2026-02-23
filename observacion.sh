35cdad52eca5aff81e5f0dd80a9076fe235fef3d
Antonio Jose Garcia Ortega <antgarort@alu.edu.gva.es>
2026-02-23 19:23:11 +0100

    Update observacion.sh

--- observacion.sh
+++ observacion.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-
 ############################################
 # CONFIGURACIÓN
 ############################################
 
+# Carpeta de imágenes
 OBSERVACION_DIR="/mnt/sistemaImagenes/observacion"
+# Carpeta para logs
 REG_LOG_DIR="/mnt/sistemaImagenes/registro_log/observacion"
-LOCAL_LOG_BASE="/home/adquisicion-datos/registro_log/observacion"
 
+# URL base
 BASE_URL="https://www.aemet.es/imagenes_d/eltiempo/observacion/satelite"
 
 ############################################
@@ -17,20 +17,16 @@ BASE_URL="https://www.aemet.es/imagenes_d/eltiempo/observacion/satelite"
 ANIO=$(date +"%Y")
 MES=$(date +"%m")
 DIA=$(date +"%d")
-HORA=$(date -d "-1 hour" +"%H")
+HORA=$(date +"%H")
 FECHA_URL="${ANIO}${MES}${DIA}${HORA}00"
 ARCHIVO="${FECHA_URL}_s93g.gif"
 
+# Rutas de destino y log
 DEST_DIR="$OBSERVACION_DIR/$ANIO/$MES/$DIA"
 DESTINO="$DEST_DIR/$ARCHIVO"
 
 LOG_DIR="$REG_LOG_DIR/$ANIO/$MES/$DIA"
-mkdir -p "$LOG_DIR"
 LOG="$LOG_DIR/${FECHA_URL}_s93g.log"
-
-LOCAL_LOG_DIR="$LOCAL_LOG_BASE/$ANIO/$MES/$DIA"
-mkdir -p "$LOCAL_LOG_DIR"
-LOCAL_LOG="$LOCAL_LOG_DIR/${FECHA_URL}_s93g.log"
 
 ############################################
 # DESCARGA
@@ -39,13 +35,9 @@ LOCAL_LOG="$LOCAL_LOG_DIR/${FECHA_URL}_s93g.log"
 wget -q --timeout=20 --tries=2 "$BASE_URL/$ARCHIVO" -O "$DESTINO"
 
 if [ $? -eq 0 ]; then
-    MENSAJE="$(date '+%Y-%m-%d %H:%M:%S') - Imagen descargada correctamente: $ARCHIVO"
-    echo "$MENSAJE" >> "$LOG"
-    echo "$MENSAJE" >> "$LOCAL_LOG"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Imagen descargada correctamente: $ARCHIVO" >> "$LOG"
 else
-    MENSAJE="$(date '+%Y-%m-%d %H:%M:%S') - ERROR descargando imagen: $ARCHIVO"
-    echo "$MENSAJE" >> "$LOG"
-    echo "$MENSAJE" >> "$LOCAL_LOG"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - ERROR descargando imagen: $ARCHIVO" >> "$LOG"
     rm -f "$DESTINO"
 fi
 
